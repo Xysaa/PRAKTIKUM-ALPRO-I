ad20f52f19676741cecc90e7b073056a04bc7e1b
Scxzxz <93330587+Xysaa@users.noreply.github.com>
2024-09-19 14:47:14 +0700

    Feat : Pertemuan I

---
+++ Latihan CPP/Pertemuan I/inputOutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    string namaSaya;
+    cin >> namaSaya;
+    cout << "Hello, " << namaSaya << "!" << endl;
+    int nim;
+    cin >> nim;
+    cout << "NIM: " << nim << endl;
+    return 0;
+}
